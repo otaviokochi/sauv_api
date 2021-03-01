84e1d51ed27e80551e8ca601e46923bad7ba0968
Gustavo <GustavoHFCruz@outlook.com>
2021-02-28 22:28:42 -0300

    CRUD funcionario e coordenador feitos e testados

--- src/controllers/coordenadorController.js
+++ src/controllers/coordenadorController.js
@@ -1,17 +1,85 @@
+const connection = require("../../knexfile");
+
 module.exports = {
     async create(request, response) {
+        const {
+            primNome,
+            sobrenome,
+            genero,
+            cpf,
+            rg,
+            email,
+            telefone,
+            logradouro,
+            pais,
+            estado,
+            cidade
+        } = request.body;
 
+        await connection("coordenador").insert({
+            primNome,
+            sobrenome,
+            genero,
+            cpf,
+            rg,
+            email,
+            telefone,
+            logradouro,
+            pais,
+            estado,
+            cidade
+        });
+
+        return response.sendStatus(200);
     },
 
     async read(request, response) {
-        
+        const [count] = await connection("coordenador").count();
+
+        const coordenadores = await connection("coordenador").select("*");
+
+        response.header("X-Total-Count", count["count(*)"]);
+
+        return response.json(coordenadores);
     },
 
     async update(request, response) {
+        const {
+            primNome,
+            sobrenome,
+            genero,
+            cpf,
+            rg,
+            email,
+            telefone,
+            logradouro,
+            pais,
+            estado,
+            cidade
+        } = request.body;
 
+        await connection("coordenador").update({
+            primNome,
+            sobrenome,
+            genero,
+            cpf,
+            rg,
+            email,
+            telefone,
+            logradouro,
+            pais,
+            estado,
+            cidade
+        })
+
+        return response.sendStatus(200);
     },
 
     async delete(request, response) {
+        const { rg } = request.body;
 
+        await connection("coordenador").delete(rg);
+
+        return response.sendStatus(200);
     },
 };
