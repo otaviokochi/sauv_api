07cb53b2f02c9158b2832dc0d46853f60d8a62a0
otaviokochi <otaviokochi@gmail.com>
2021-03-10 20:27:09 -0300

    tirando minha conexao banco de dados

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ module.exports = {
     development: {
       client: 'mysql',
       connection: {
-        host: "localhost",
-        user: "root", 
-        password: "otaviokochi",
-        database: "sauv"
+        host: "",
+        user: "", 
+        password: "",
+        database: ""
       },
       migrations: {
         tableName: 'knex_migrations',
