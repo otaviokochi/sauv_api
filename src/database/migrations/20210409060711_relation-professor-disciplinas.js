58dbf4ade61e0c39e56953b71fd88f7cfb533ba6
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-04-15 06:31:26 -0300

    Ajusta as relacoes entre professor e disciplina e cria a tabela de frequencia de aluno

--- src/database/migrations/20210409060711_relation-professor-disciplinas.js
+++ src/database/migrations/20210409060711_relation-professor-disciplinas.js
@@ -1,9 +1,5 @@
 
-exports.up = knex => knex.schema.createTable('relation-professor-disciplina', table => {
-  table.increments('id').primary();
-  table.integer('professor_id');
-  table.integer('disciplina_id');
-}).createTable('professores', table => {
+exports.up = knex => knex.schema.createTable('professores', table => {
   table.increments('id').primary();
   table.string('nome').notNullable();
   table.string('cpf').notNullable();
@@ -15,6 +11,12 @@ exports.up = knex => knex.schema.createTable('relation-professor-disciplina', ta
   table.string('bairro').notNullable();
   table.string('endereco').notNullable();
   table.string('complemento').notNullable();
-});;
+}).createTable('relation-professor-disciplina', table => {
+  table.increments('id').primary();
+  table.integer('professor_id').unsigned();
+  table.integer('disciplina_id').unsigned();
+  table.foreign('professor_id').references('id').inTable('professores');
+  table.foreign('disciplina_id').references('id').inTable('disciplinas');
+});
 
 exports.down = knex => knex.schema.dropTable('professores').dropTable('relation-professor-disciplina');
